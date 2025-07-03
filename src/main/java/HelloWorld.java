65ef40b062a733556a7f77125af0f801f989f96d
Sameer Patil <101757605+sameerr-patil@users.noreply.github.com>
2025-07-03 16:34:15 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,8 @@
-import spark.servlet.SparkApplication;
+//import spark.servlet.SparkApplication;
 
-import static spark.Spark.get;
+//mport static spark.Spark.get;
 
-public class HelloWorld implements SparkApplication {
+//public class HelloWorld implements SparkApplication {
 	public static void main(String[] args) {
 		new HelloWorld().init();
 	}
